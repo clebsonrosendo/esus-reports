fc66cab05550474ab78c02f27de0231e2f7bc993
Clebson Rosendo <clebsondev@gmail.com>
2024-11-06 15:05:29 -0300

    Create new Pages

---
+++ class/connection.py
@@ -0,0 +1,7 @@
+import sqlite3 as sqlite
+
+# Classe de Inicialização do Banco de dados
+class DataBase:
+
+    path = './.streamlit/'
+
---
+++ pages/Report_Professional.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+
+st.title('Relatório Profissional')
