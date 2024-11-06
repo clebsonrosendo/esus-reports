c652871e1b358c211143ba0346aec9768c316a9c
Clebson Rosendo <clebsondev@gmail.com>
2024-11-06 14:59:55 -0300

    Insert Menu with Pages

--- Home.py
+++ Home.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
 
-# Config page
-st.set_page_config(page_title="PÁGINA INICIAL", page_icon=":material/home:", )
-st.title("PÁGINA INICIAL")
+connection_page = st.Page(
+    'pages/Connection.py',
+    title='Gerenciador de Conexões',
+    icon=':material/database:')
 
+professional_page = st.Page(
+    'pages/Report_Professional.py',
+    title="Relatórios Profissional",
+    icon=":material/lab_profile:")
+
+
+pg = st.navigation([professional_page, connection_page])
+
+# Config page
+st.set_page_config(page_title="PÁGINA INICIAL", page_icon=":material/home:")
+pg.run()
