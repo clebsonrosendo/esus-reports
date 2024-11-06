3eb3a8d85198b334f1fd6a45af0f8a269b9aed67
Clebson Rosendo <clebsondev@gmail.com>
2024-11-06 15:16:38 -0300

    Alter icon pages

--- Home.py
+++ Home.py
@@ -8,12 +8,37 @@ connection_page = st.Page(
 
 professional_page = st.Page(
     'pages/Report_Professional.py',
-    title="Relatórios Profissional",
-    icon=":material/lab_profile:")
+    title='Relatórios Profissional',
+    icon=':material/analytics:')
+
+services_page = st.Page(
+    'pages/Atendimentos.py',
+    title='Atendimentos Profissional',
+    icon=':material/groups:')
+
+evolution_page = st.Page(
+    'pages/Evolucoes.py',
+    title='Evoluções',
+    icon=':material/how_to_reg:')
+
+time_page = st.Page(
+    'pages/Horarios.py',
+    title='Horários dos Profissionais',
+    icon=':material/pending_actions:')
+
+managerbk_page = st.Page(
+    'pages/Gerenciador_Backups.py',
+    title='Gerenciador de Backups',
+    icon=':material/cloud_upload:')
 
 
-pg = st.navigation([professional_page, connection_page])
+pg = st.navigation([services_page,
+                    professional_page,
+                    evolution_page,
+                    time_page,
+                    managerbk_page,
+                    connection_page])
 
 # Config page
-st.set_page_config(page_title="PÁGINA INICIAL", page_icon=":material/home:")
+st.set_page_config(page_title='PÁGINA INICIAL', page_icon=':material/home:')
 pg.run()
