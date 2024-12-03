81726b76bcb157dd2b444953a3ed20a59a19fdeb
Clebson Rosendo <clebsondev@gmail.com>
2024-12-03 17:13:30 -0300

    fix(atendimentos): test new df

--- pages/atendimentos.py
+++ pages/atendimentos.py
@@ -10,17 +10,17 @@ st.warning('Recurso Indisponível no Momento')
 
 
 file = open("../querys/atendimentos_prof.sql", "r")
-file = str(file.read())
+query = str(file.read())
 
 # Get data DB
-query = conn.new(file)
+# result_query = conn.new(query)
 # Generate DataFrame
-df = pd.DataFrame(query)
+# df = pd.DataFrame(result)
 # Date Filter
-select_date_i = st.selectbox('Data', options=df['Data'].unique(), index=None, placeholder='Escolha um dia', format="DD/MM/YYYY")
-select_date_f = st.selectbox('Data', options=df['Data'].unique(), index=None, placeholder='Escolha um dia', format="DD/MM/YYYY")
-
-# Show Button
-if select_date:
-    df_filtered = df[df["Data"] >= select_date_i & df["Data"] <= select_date_f]
-    st.dataframe(df_filtered, hide_index=False)
+# select_date_i = st.selectbox('Data', options=df['Data'].unique(), index=None, placeholder='Escolha um dia', format="DD/MM/YYYY")
+# select_date_f = st.selectbox('Data', options=df['Data'].unique(), index=None, placeholder='Escolha um dia', format="DD/MM/YYYY")
+#
+# # Show Button
+# if select_date:
+#     df_filtered = df[df["Data"] >= select_date_i & df["Data"] <= select_date_f]
+#     st.dataframe(df_filtered, hide_index=False)
